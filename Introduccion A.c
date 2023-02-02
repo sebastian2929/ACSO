692ffb8498f1ee23bd7c4717caec5679bcca6a61
sebastian2929 <62757013+sebastian2929@users.noreply.github.com>
2023-02-02 14:16:34 -0500

    Update Introduccion A.c

--- Introduccion A.c
+++ Introduccion A.c
@@ -2,17 +2,15 @@
 
 int main() {
     int veces;
-    float num1, num2, resultado;
-    char op;
-    int valido = 1;
+    int num1, num2, resultado;
     
     scanf("%i%*c", &veces);
     while(veces > 0){
-        scanf("%f%*c", &num1);
-        scanf("%f%*c", &num2);
+        scanf("%i%*c", &num1);
+        scanf("%i%*c", &num2);
         resultado = num1 + num2;
         veces--;
-        printf("El resultado es %f\n", resultado);
+        printf("%i\n", resultado);
     }
     return 0;
 }
